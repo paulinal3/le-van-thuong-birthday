5fe3650e1d71f3de694ee3621249a4b203a24f4b
Paulina <paulina.le@slalom.com>
2025-04-07 16:12:21 -0400

    more updates

--- assets/js/guests.js
+++ assets/js/guests.js
@@ -1,38 +1,45 @@
 export const guests = [
     { 
         phones: ["7044416580"],
-        name: "Thang Lê", 
-        invitees: ["Thomas Lê", "Mignon Lê", "Michael Lê", "Christina Lê", "Paulina Lê", "Lee Kaufman"]
+        name: "Thăng Lê", 
+        invitees: ["Thomas Lê", "Mignon Lê", "Michael Lê", "Christina Lê", "Paulina Lê", "Lee Kaufman"],
+        isImmediateFam: true
     }, 
     {
         phones: ["7032824957"],
         name: "Thúy Lê", 
-        invitees: ["Thúy Lê", "Titi Vũ", "Christopher Vũ", "Kayla Vũ", "Mai-Ly Vũ", "Brice Vũ"]
+        invitees: ["Thúy Lê", "Titi Vũ", "Christopher Vũ", "Kayla Vũ", "Mai-Ly Vũ", "Brice Vũ"],
+        isImmediateFam: true
     }, 
     { 
         phones: ["7039634022", "7035097016"],
         name: "Thư Trần", 
-        invitees: ["Thu Trần", "Hoàng Trần", "Vinh Trần"]
+        invitees: ["Thu Trần", "Hoàng Trần", "Vinh Trần"],
+        isImmediateFam: true
     },
     { 
         phones: ["7036243576"],
         name: "Thương Lê", 
-        invitees: ["Thương Lê", "Mina Bùi", "Huy Lâm", "Audrey Lâm", "Adeline Lâm", "Ti Bùi"] 
+        invitees: ["Thương Lê", "Mina Bùi", "Huy Lâm", "Audrey Lâm", "Adeline Lâm", "Ti Bùi", "Curtis Flickinger"],
+        isImmediateFam: true
     }, 
     { 
         phones: ["5712475533", "5712475534"],
         name: "Thùy Lê", 
-        invitees: ["Thùy Lê", "Dũng Nguyễn", "Paul Nguyễn", "Mary Nguyễn", "Bin Nguyễn"] 
+        invitees: ["Thùy Lê", "Dũng Nguyễn", "Paul Nguyễn", "Mary Nguyễn", "Bin Nguyễn"],
+        isImmediateFam: true
     },
     { 
         phones: ["7036255560"],
         name: "Thạch Lê", 
-        invitees: ["Thạch Lê", "Thanh Lê", "Chloe Lê", "Tiana Lê", "Theresa Lê"] 
+        invitees: ["Thạch Lê", "Thanh Lê", "Chloe Lê", "Tiana Lê", "Theresa Lê"],
+        isImmediateFam: true
     },
     { 
         phones: ["7037959749"],
         name: "Lụư Lê", 
-        invitees: ["Lụư Lê", "Logan Lê"] 
+        invitees: ["Lụư Lê", "Logan Lê"],
+        isImmediateFam: true
     }, 
     { 
         phones: ["9192723956"],
@@ -47,12 +54,12 @@ export const guests = [
     { 
         phones: ["4437658307"],
         name: "Hiếu Trương", 
-        invitees: ["Hiếu Trương", "Tracy <>", "Kid 1", "Kid 2"] 
+        invitees: ["Hiếu Trương", "Tracy", "Kid 1", "Kid 2"] 
     }, 
     { 
         phones: ["7036250217", "7036246317"],
         name: "Ông Bà", 
-        invitees: ["Ông Bà"] 
-    }  
-
+        invitees: ["Ông Bà"],
+        isImmediateFam: true
+    }
 ];
--- assets/js/rsvp.js
+++ assets/js/rsvp.js
@@ -1,5 +1,4 @@
-import { guests } from './guests.js';
-
+// import { guests } from './guests.js';
 document.addEventListener('DOMContentLoaded', function() {
     // Confetti effect on page load
     setTimeout(function() {
@@ -10,63 +9,226 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     }, 1000);
 
+    const rsvpNavButton = document.getElementById('rsvpNavButton');
     const rsvpModal = document.getElementById('rsvpModal');
     const closeModal = document.querySelector('.close');
-    const submitPhone = document.getElementById('submitPhone');
-    const guestPhoneInput = document.getElementById('guestPhone');
-    const phoneInputSection = document.getElementById('phoneInputSection');
+    const submitPhoneButton = document.getElementById('submitPhoneButton');
+    const guestPhoneInput = document.getElementById('guestPhoneInput');
+    const phoneInputContainer = document.getElementById('phoneInputContainer');
     const partyDetails = document.getElementById('partyDetails');
     const partyName = document.getElementById('partyName');
     const inviteesList = document.getElementById('inviteesList');
+    const privateCelebration = document.getElementById('privateCelebration');
+    const rsvpActions = document.getElementById('rsvpActions');
 
-    // Ong Noi's Address
-    // Pre-birthday celebration (immediate family only)
-    // 3:00-6:30pm
-    // 6:30pm head to restaurant
-    // 40 min drive to Texas
-    // Dinner 7:30-9:30
+    const guests = [
+        { 
+            phones: ["7044416580"],
+            name: "Thăng Lê", 
+            invitees: ["Thomas Lê", "Mignon Lê", "Michael Lê", "Christina Lê", "Paulina Lê", "Lee Kaufman"],
+            isImmediateFam: true
+        }, 
+        {
+            phones: ["7032824957"],
+            name: "Thúy Lê", 
+            invitees: ["Thúy Lê", "Titi Vũ", "Christopher Vũ", "Kayla Vũ", "Mai-Ly Vũ", "Brice Vũ"],
+            isImmediateFam: true
+        }, 
+        { 
+            phones: ["7039634022", "7035097016"],
+            name: "Thư Trần", 
+            invitees: ["Thu Trần", "Hoàng Trần", "Vinh Trần"],
+            isImmediateFam: true
+        },
+        { 
+            phones: ["7036243576"],
+            name: "Thương Lê", 
+            invitees: ["Thương Lê", "Mina Bùi", "Huy Lâm", "Audrey Lâm", "Adeline Lâm", "Ti Bùi", "Curtis Flickinger"],
+            isImmediateFam: true
+        }, 
+        { 
+            phones: ["5712475533", "5712475534"],
+            name: "Thùy Lê", 
+            invitees: ["Thùy Lê", "Dũng Nguyễn", "Paul Nguyễn", "Mary Nguyễn", "Bin Nguyễn"],
+            isImmediateFam: true
+        },
+        { 
+            phones: ["7036255560"],
+            name: "Thạch Lê", 
+            invitees: ["Thạch Lê", "Thanh Lê", "Chloe Lê", "Tiana Lê", "Theresa Lê"],
+            isImmediateFam: true
+        },
+        { 
+            phones: ["7037959749"],
+            name: "Lụư Lê", 
+            invitees: ["Lụư Lê", "Logan Lê"],
+            isImmediateFam: true
+        }, 
+        { 
+            phones: ["9192723956"],
+            name: "Thủy Tíên Lê", 
+            invitees: ["Thủy Tíên Lê", "Gina Lê"] 
+        }, 
+        { 
+            phones: ["6785714245"],
+            name: "Thảo Trương", 
+            invitees: ["Tùng Trương", "Thảo Trương", "John Barker", "Erin Barker", "Mya Barker"] 
+        }, 
+        { 
+            phones: ["4437658307"],
+            name: "Hiếu Trương", 
+            invitees: ["Hiếu Trương", "Tracy", "Kid 1", "Kid 2"] 
+        }, 
+        { 
+            phones: ["7036250217", "7036246317"],
+            name: "Ông Bà", 
+            invitees: ["Ông Bà"],
+            isImmediateFam: true
+        }
+    ];
+
+    rsvpNavButton.onclick = function() {
+        // Reset modal to initial state
+        guestPhoneInput.value = '';
+        phoneInputContainer.style.display = "block";
+        partyDetails.style.display = "none";
+        rsvpModal.style.display = "block";
+        rsvpActions.style.display = "none";
+        privateCelebration.style.display = "none";
+    };
 
     closeModal.onclick = function() {
         rsvpModal.style.display = "none";
     };
 
-    submitPhone.onclick = function() {
+    submitPhoneButton.onclick = function() {
         const guestPhone = guestPhoneInput.value;
         const guestInfo = guests.find(guest => guest.phones.includes(guestPhone));
+        guestInfo.isImmediateFam ? privateCelebration.style.display = "block" : privateCelebration.style.display = "none";
+
         if (guestInfo) {
-            partyName.textContent = `RSVP for ${guestInfo.name} household`;
+            partyName.innerHTML = `Respond by <u style="color: #f41818">May 15</u> for <span style="color: #f4c118">${guestInfo.name}</span> household`;
             inviteesList.innerHTML = '';
             guestInfo.invitees.forEach(invitee => {
-                const listItem = document.createElement('li');
-                listItem.innerHTML = `${invitee} 
-                                      <button class="actions" onclick="updateStatus('${invitee}', 'accept')">Accept</button>
-                                      <button class="actions" onclick="updateStatus('${invitee}', 'decline')">Decline</button>
-                                      <select class="field half>
-                                          <option value="">Age</option>
-                                          <option value="adult">Adult</option>
-                                          <option value="child">Child (6-12yrs)</option>
-                                          <option value="toddler">Toddler (3-5yrs)</option>
-                                          <option value="baby">Baby (under 2yrs)</option>
-                                      </select>`;
+                const listItem = document.createElement('div');
+                listItem.classList.add('inviteeContainer')
+                listItem.innerHTML = `<p class='inviteeName'>${invitee}</p>
+                                      <div class='rsvpActionButtons'>
+                                        <button class="btn btn-unselected small" onclick="updateStatus(this, '${invitee}', 'accept')">Accept</button>
+                                        <select class="form-control hidden small">
+                                            <option value="">Select Age</option>
+                                            <option value="adult">Adult</option>
+                                            <option value="child">Child (6-12yrs)</option>
+                                            <option value="toddler">Toddler (3-5yrs)</option>
+                                            <option value="baby">Baby (under 2yrs)</option>
+                                        </select>
+                                        <button class="btn btn-unselected small" onclick="updateStatus(this, '${invitee}', 'decline')">Decline</button>
+                                      </div>
+                                      <span class="error-message" style="color: red;"></span>`;
                 inviteesList.appendChild(listItem);
             });
-            phoneInputSection.style.display = "none";
-            partyDetails.style.display = "block";
-            nameInputContainer.style.display = "none";
+            phoneInputContainer.style.display = "none";
             partyDetails.style.display = "block";
             rsvpActions.style.display = "flex";
         } else {
-            alert('Name not found.');
+            alert('Phone number not found.');
         }
     };
+
+    window.updateStatus = function(button, invitee, status) {
+        const buttons = button.parentElement.querySelectorAll('button');
+        const dropdown = button.parentElement.querySelector('select');
+        let errorMessage = button.parentElement.parentElement.querySelector('.error-message');
+
+        buttons.forEach(btn => {
+            if (btn === button) {
+                btn.classList.add('btn-selected');
+                btn.classList.remove('btn-unselected');
+                btn.textContent = status === 'accept' ? 'Accepted' : 'Declined';
+            } else {
+                btn.classList.remove('btn-selected');
+                btn.classList.add('btn-unselected');
+                btn.textContent = btn.textContent.includes('Accept') ? 'Accept' : 'Decline';
+            }
+        });
+
+        if (status === 'accept') {
+            dropdown.classList.remove('hidden');
+            if (dropdown.value === '') {
+                errorMessage.textContent = 'Please select an age.';
+            }
+        } else {
+            dropdown.classList.add('hidden');
+            errorMessage.textContent = ''; // Clear error if Decline is selected
+        }
+    }
+
+    inviteesList.addEventListener('change', function(event) {
+        if (event.target.tagName.toLowerCase() === 'select') {
+            const dropdown = event.target;
+            let errorMessage = dropdown.parentElement.parentElement.querySelector('.error-message');
+            if (dropdown.value !== '') {
+                errorMessage.textContent = ''; // Clear error if an age is selected
+            }
+        }
+    });
 
     window.onclick = function(event) {
         if (event.target === rsvpModal) {
             rsvpModal.style.display = "none";
         }
     };
-});
 
-function updateStatus(invitee, status) {
-    console.log(`Invitee: ${invitee}, Status: ${status}`);
-}
+    document.getElementById('rsvpActions').addEventListener('click', function(event) {
+        if (event.target && event.target.value === "Send RSVP") {
+            if (validateRSVP()) sendRSVPEmail();
+        }
+    });
+
+    function validateRSVP() {
+        let valid = true;
+
+        inviteesList.querySelectorAll('.inviteeContainer').forEach(container => {
+            const statusButton = container.querySelector('.btn-selected');
+            const dropdown = container.querySelector('select');
+            let errorMessage = container.querySelector('.error-message');
+
+            if (!statusButton) {
+                errorMessage.textContent = 'Please select Accept or Decline.';
+                valid = false;
+            } else if (statusButton.textContent === 'Accepted' && dropdown.value === '') {
+                errorMessage.textContent = 'Please select an age.';
+                valid = false;
+            } else {
+                errorMessage.textContent = ''; // Clear error message if valid
+            }
+        });
+
+        return valid;
+    }
+
+    function sendRSVPEmail() {
+        const guestName = partyName.textContent.match(/for (.*?) household/)[1];
+        const rsvpDetails = [];
+        inviteesList.querySelectorAll('.inviteeContainer').forEach(container => {
+            const name = container.querySelector('.inviteeName').textContent;
+            const status = container.querySelector('.btn-selected').textContent;
+            const age = container.querySelector('select').value;
+            rsvpDetails.push({ name, status, age });
+        });
+
+        const templateParams = {
+            householdName: guestName,
+            rsvpDetails: JSON.stringify(rsvpDetails, null, 2) // Format as JSON
+        };
+
+        console.log(`Email details: ${JSON.stringify(templateParams, null, 2)}`);
+
+        emailjs.send("service_ni9np5g", "90_rsvp", templateParams)
+            .then(function(response) {
+                alert('RSVP sent successfully!');
+            }, function(error) {
+                alert('Failed to send RSVP: ' + error.text);
+            });
+    }
+});
